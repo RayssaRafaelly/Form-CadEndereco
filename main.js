c74a1442eb8f05bb003b079a58603dec48de8b17
Rayssa Rafaelly <rayssa.kopinski@alunoescolassociais.org.br>
2023-09-29 09:20:16 -0300

    criado arquivo de java e feito função limpar formulario

---
+++ main.js
@@ -0,0 +1,11 @@
+'user strict'; //Modo strict mode ou Modo restrito 
+//https://viacep.com.br/
+
+//Função para limpar formulário 
+const limparformulario = () => {
+      document.getElementById('rua').value = '';
+      document.getElementById('bairro').value = '';
+      document.getElementById('cidade').value = '';
+      document.getElementById('estado').value = '';
+
+}
